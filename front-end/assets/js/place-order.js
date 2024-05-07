f7f3cbd57a121565fb6deb5d4332f1d6194339d4
duvindu111 <duvinduthathsara@gmail.com>
2024-05-07 15:44:59 +0530

    going on

--- front-end/assets/js/place-order.js
+++ front-end/assets/js/place-order.js
@@ -146,88 +146,186 @@ $("#btn_add_cart").click(function (){
         alert("Please select an item first")
     }
 
-    let finalTotal = 0;
+    let finalTotal = getTotalAfterDiscount();
+    $("#total_price").text(finalTotal);
+});
+
+$("#btn_confirm").click(function(){
+   // if($("#tableCartBody tr").length > 0){
+   //     if($("#payment_method").val() != null){
+   //         if($("#name_of_user").text() != ""){
+   //             if($("#code_of_employee").text() != ""){
+                   ////////////////////////////////////////////////
+                   if($("#payment_method").val() == "CARD"){
+                          $("#pm_card_modal").modal('show');
+                   }else if($("#payment_method").val() == "CASH"){
+                          $("#pm_cash_modal").modal('show');
+                   }
+                   ////////////////////////////////////////////////
+   //             }else{
+   //                 alert("Seems to be a problem with the user account. Please check again.")
+   //             }
+   //         }else{
+   //             alert("Seems to be a problem with the user account. Please check again.")
+   //         }
+   //     }else{
+   //         alert("Please select a payment method");
+   //     }
+   // }else{
+   //     alert("Please add items to the cart first");
+   // }
+});
+
+$(".btn_place_order").click(function (){
+    if(paymentCardValidation()){
+        let order_id = $("#order_id").text();
+        let order_date = new Date();
+        let cust_code = $("#cust_code").val();
+        let cust_name = $("#cust_name").val();
+        let total = $("#total_price").text();
+        let added_points = $("#added_points").val();
+        let payment_method = $("#payment_method").val();
+        let employee_code = $("#code_of_employee").text();
+        let cashier_name = $("#name_of_user").text();
+
+        console.log(order_date);
+
+        let orderDetails = [];
+        $("#tableCartBody tr").each(function() {
+            let orderDetail = {
+                orderId: order_id,
+                itemCode: $(this).find("td:eq(0)").text(),
+                itemName: $(this).find("td:eq(1)").text(),
+                unitPrice: $(this).find("td:eq(3)").text(),
+                size: $(this).find("td:eq(2)").text(),
+                qty: $(this).find("td:eq(4)").text()
+            }
+            orderDetails.push(orderDetail);
+        });
+
+        $.ajax({
+            url: 'http://localhost:8080/hello_shoes/api/v1/place_order/placeOrder',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({"orderId": order_id, "orderDate": order_date, "customer": cust_code, "customerName": cust_name, "totalPrice": total,
+                "addedPoints": added_points, "paymentMethod": payment_method, "employee": employee_code, "cashierName": cashier_name, "orderDetailList": orderDetails
+            }),
+            success: function (response) {
+                alert("Order placed successfully.")
+                resetPlaceOrderPage();
+                $("#pm_card_modal").modal('hide');
+                console.log(response);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert("Order not placed. Please check again.")
+                console.error(jqXHR);
+            }
+        });
+    }
+});
+
+$("#added_points").keyup(function (){
+   $("#total_price").text(getTotalAfterDiscount());
+});
+
+$(".btn_next").click(function (){
+   $("#cash_header").text("Order ID: " + $("#order_id").text());
+   $(".admin-details").css("display", "none");
+
+   $("#cash_total").val($("#total_price").text());
+   $(".cash-details").css("display", "flex");
+});
+
+$("#paid_amount").keyup(function(){
+    let total = $("#cash_total").val();
+    let paid_amount = $("#paid_amount").val();
+    let balance = paid_amount - total;
+    $("#balance").val(balance);
+
+    console.log(balance);
+    if(balance >= 0){
+        $(".cash-details .btn_place_order").prop("disabled", false);
+    }else{
+        $(".cash-details .btn_place_order").prop("disabled", true);
+    }
+});
+
+function resetPlaceOrderPage(){
+    generateNextOrderId();
+    $("#item_code").val("");
+    $("#item_size").val("");
+    $("#buying_qty").val("");
+    $("#available_qty").text("");
+    $("#product_img").attr("src", "");
+    $("#item_name").text("");
+    $("#unit_price").text("");
+    $("#added_points").val("");
+    $("#total_price").text("");
+    $("#cust_code").val("");
+    $("#cust_name").text("");
+    $("#cust_email").text("");
+    $("#cust_contact").text("");
+    $("#cust_level").text("");
+    $("#cust_points").text("");
+    $("#payment_method").val("");
+    $("#tableCartBody").empty();
+    $(".item-related-block").css("display", "none");
+    $(".item-related-flex").css("display", "none");
+    $(".cust-related-flex").css("display", "none");
+
+}
+
+function paymentCardValidation(){
+let card_no = $("#pm_card_no").val();
+    let card_num = $("#card_num").val();
+    let bank_name = $("#bank_name").val();
+    let exp_date = $("#exp_date").val();
+    let cvv = $("#cvv").val();
+    let name_on_card = $("#name_on_card").val();
+
+    if(card_num == "" || bank_name == "" || exp_date == "" || cvv == "" || name_on_card == ""){
+        alert("Please fill in all the fields");
+        return false;
+    }else{
+        let card_no_regex = new RegExp("^[0-9]{3,4}$");
+        let cvv_regex = new RegExp("^[0-9]{3,4}$");
+        let name_on_card_regex = new RegExp("^^[a-zA-Z ]{2,30}$");
+
+        if(!card_no_regex.test(card_num)){
+            alert("Invalid card number");
+            return false;
+        }else if(!cvv_regex.test(cvv)){
+            alert("Invalid card cvc");
+            return false;
+        }else if(!name_on_card_regex.test(name_on_card)){
+            alert("Invalid name");
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
+
+function getTotalAfterDiscount(){
+    let total = 0;
     $("#tableCartBody tr").each(function() {
         let row_unit_price = parseFloat($(this).find("td:eq(3)").text());
         let row_qty = parseInt($(this).find("td:eq(4)").text());
         let row_total = row_unit_price * row_qty;
-        finalTotal = finalTotal + row_total;
+        total = total + row_total;
     });
-    $("#total_price").text(finalTotal);
-
-    // let discount = $("#o_inputDiscount").val();
-    // if(discount===""){
-    //     subTotal = finalTotal;
-    // }else{
-    //     let reduced_amount = (finalTotal/100)*parseFloat(discount);
-    //     subTotal = finalTotal-reduced_amount;
-    // }
-    // $("#o_lblSubTotal").html("&nbsp;" + subTotal + "/=");
-});
-
-$("#btn_confirm").click(function(){
-   if($("#tableCartBody tr").length > 0){
-       if($("#payment_method").val() != null){
-           if($("#name_of_user").text() != ""){
-               if($("#code_of_employee").text() != ""){
-                   ////////////////////////////////////////////////
-               }else{
-                   alert("Seems to be a problem with the user account. Please check again.")
-               }
-           }else{
-               alert("Seems to be a problem with the user account. Please check again.")
-           }
-       }else{
-           alert("Please select a payment method");
-       }
-   }else{
-       alert("Please add items to the cart first");
-   }
-});
-
-$(".btn_place_order").click(function (){
-   let order_id = $("#order_id").text();
-   let order_date = new Date();
-   let cust_code = $("#cust_code").val();
-   let cust_name = $("#cust_name").val();
-   let total = $("#total_price").text();
-   let added_points = $("#added_points").val();
-   let payment_method = $("#payment_method").val();
-   let employee_code = $("#code_of_employee").text();
-   let cashier_name = $("#name_of_user").text();
-
-    console.log(order_date);
-
-   let orderDetails = [];
-   $("#tableCartBody tr").each(function() {
-       let orderDetail = {
-              orderId: order_id,
-              itemCode: $(this).find("td:eq(0)").text(),
-              itemName: $(this).find("td:eq(1)").text(),
-              unitPrice: $(this).find("td:eq(3)").text(),
-              size: $(this).find("td:eq(2)").text(),
-              qty: $(this).find("td:eq(4)").text()
-       }
-       orderDetails.push(orderDetail);
-   });
-
-    $.ajax({
-        url: 'http://localhost:8080/hello_shoes/api/v1/place_order/placeOrder',
-        method: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({"orderId": order_id, "orderDate": order_date, "customer": cust_code, "customerName": cust_name, "totalPrice": total,
-            "addedPoints": added_points, "paymentMethod": payment_method, "employee": employee_code, "cashierName": cashier_name, "orderDetailList": orderDetails
-        }),
-        success: function (response) {
-            console.log(response);
-
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            alert(jqXHR.responseJSON.message);
-            console.error(jqXHR);
-        }
-    });
-});
+    let added_points = $("#added_points").val();
+    console.log(added_points);
+    if(added_points == "" ){
+        return total;
+    }else{
+        added_points = parseInt(added_points);
+        let discount = (total/100) * added_points;
+        let new_total = total - discount;
+        new_total = new_total.toFixed(2);
+        return new_total;
+    }
+}
 
 function removeIconOnClicked(){
     $(".cart-remove-btn:last").click(function (){
